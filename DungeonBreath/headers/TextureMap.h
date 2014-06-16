59b9d0db555fdf829c82f638743f5be7d774be73
Nick <nick@Pangolin.(none)>
2014-06-15 19:44:52 -0700

    Added TextureMap

---
+++ DungeonBreath/headers/TextureMap.h
@@ -0,0 +1,22 @@
+#ifndef TEXTUREMAP_H
+#define TEXTUREMAP_H
+
+#include <SFML/Graphics.hpp>
+#include <map>
+#include <string>
+#include <iostream>
+
+class TextureMap
+{
+ public:
+ 	TextureMap();
+ 	~TextureMap();
+ 	
+ 	sf::Texture* request(std::string file_name);
+ 	void clear();
+ private:
+ 	static std::map<std::string, sf::Texture> tex_map;
+ 
+};
+
+#endif //TEXTUREMAP_H
