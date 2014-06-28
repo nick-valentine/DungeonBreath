d5e38858766b62ade6ed97779604e1cc234334bd
nick-valentine <nick9625@gmail.com>
2014-06-27 19:52:30 -0700

    having some fun with loading from files

--- VS_Proj/DungeonBreath/sources/GameScene.cpp
+++ VS_Proj/DungeonBreath/sources/GameScene.cpp
@@ -23,54 +23,8 @@ void GameScene::init(int width, int height)
     srand( time( NULL ) );
 	std::vector<Actor *> myActors;
 	my_actors.push_back(new Hero);
-	dynamic_cast<Hero*>(my_actors[my_actors.size() - 1])->init(100,-100,50,50);
-	/*
-	EnemyFollower *Dolly = new EnemyFollower;
-	Dolly->init(500, 400, 50, 50);
-	Dolly->set_collide_type(Actor::Nothing);
-	Dolly->unregister();
-	EnemyFactory.init(Dolly, 300000, 1500, 30, 10);
-	
-	EnemyFollower *Dolly2 = new EnemyFollower;
-	Dolly2->init(600, 300, 50, 50);
-	Dolly2->set_collide_type(Actor::Nothing);
-	Dolly2->unregister();
-	EnemyFactory.add_actor(Dolly2, 300000, 3000);
-	
-	Trigger *my_trigger = new Trigger;
-	my_trigger->init(500, 100, 100, 100, Trigger::Latch, Actor::Player);
-	EnemyFactory.set_trigger(my_trigger);
-	
-	EnemyFollower *test = new EnemyFollower;
-	test->init(800, 200, 50, 50);
-	my_actors.push_back(test);
+	dynamic_cast<Hero*>(my_actors[my_actors.size() - 1])->init(50,50,50,50);
 
-	my_actors.push_back(test->clone());
-	
-	for(int i = 0; i < 10; ++i)
-	{
-	    my_actors.push_back(new Wall);
-	    dynamic_cast<Wall*>(my_actors[my_actors.size() - 1])->init(50 * i, 0, 50, 50, "./img/BrickWall.png");
-	
-    	my_actors.push_back(new Wall);
-	    dynamic_cast<Wall*>(my_actors[my_actors.size() - 1])->init(50 * i, 650, 50, 50, "./img/BrickWall.png");
-	    
-	    my_actors.push_back(new Wall);
-	    dynamic_cast<Wall*>(my_actors[my_actors.size() - 1])->init(1230, i * 50, 50, 50, "./img/BrickWall.png");
-	    
-	    my_actors.push_back(new Wall);
-	    dynamic_cast<Wall*>(my_actors[my_actors.size() - 1])->init(0, i * 50, 50, 50, "./img/BrickWall.png");
-	}
-	
-	for(int i = -15; i < 45; ++i)
-	{
-		for(int j = -15; j < 45; ++j)
-		{
-			my_tiles.push_back(new Tile);
-			dynamic_cast<Tile*>(my_tiles[my_tiles.size() - 1])->init(50 * i, 50 * j, 50, 50, (i % 2) + 1, 1);
-		}
-	}
-	*/
 	my_file.init("./gamedata/levels/level_one.txt", file_data);
 	my_file.QueueRead();
 	while(!my_file.ready())
@@ -145,7 +99,6 @@ void GameScene::init(int width, int height)
 		while(str.good())
 		{
 			str>>substr;
-			//std::cout<<substr<<std::endl;
 			
 			if(substr == "sides")
 			{
